b4e64e8cdc74e40568bf7b0ab9cca6279a367341
Baptiste Grenier <bgrenier@gnubila.fr>
2015-11-16 15:07:51 +0100

    letschat::db: allow to disable repository management

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,13 +1,14 @@
 class letschat::db (
-  $user           = $letschat::params::db_user,
-  $pass           = $letschat::params::db_pass,
-  $bind_ip        = $letschat::params::mongo_bind_address,
-  $database_name  = $letschat::params::db_name,
-  $database_port  = $letschat::params::db_port,
+  $user                = $letschat::params::db_user,
+  $pass                = $letschat::params::db_pass,
+  $bind_ip             = $letschat::params::mongo_bind_address,
+  $database_name       = $letschat::params::db_name,
+  $database_port       = $letschat::params::db_port,
+  $manage_package_repo = $letschat::params::manage_package_repo,
 ) inherits letschat::params {
 
   class { '::mongodb::globals':
-    manage_package_repo => true,
+    manage_package_repo => $manage_package_repo,
     bind_ip             => $bind_ip,
   }->
 
